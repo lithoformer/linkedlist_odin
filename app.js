70ab2a389e3c8c44907e1f13245e8b524425780a
zhiguang <zhiguang53846@gmail.com>
2024-09-29 00:39:18 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,131 @@
+class Node {
+    constructor(value = null, nextNode = null) {
+        this.value = value;
+        this.nextNode = nextNode;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        let head = new Node(null, null);
+        head.value = null;
+        head.nextNode = null;
+    }
+
+    prepend(value) {
+        if (this.head === undefined) {
+            this.head = new Node(value, null);
+        }
+        else {
+            this.head = new Node(value, this.head);
+        }
+    }
+
+    append(value) {
+        let node = new Node(value, null);
+        let traverse = this.head;
+        if (traverse === undefined) {
+            this.prepend(value);
+        }
+        else {
+            while (traverse.nextNode !== null) {
+                traverse = traverse.nextNode;
+            }
+            traverse.nextNode = node;
+        }
+    }
+
+    size() {
+        let traverse = this.head;
+        let count = 0;
+        while (traverse != null) {
+            traverse = traverse.nextNode;
+            count++;
+        }
+        return count;
+    }
+
+    getHead() {
+        return this.head.value;
+    }
+
+    tail() {
+        let traverse = this.head;
+        while (traverse.nextNode != null) {
+            traverse = traverse.nextNode;
+        }
+        return traverse.value;
+    }
+
+    at(index) {
+        let traverse = this.head;
+        for (let i = 0; i < index; i++) {
+            traverse = traverse.nextNode;
+        }
+        return traverse.value;
+    }
+
+    pop() {
+        let traverse = this.head;
+        while (traverse.nextNode.nextNode !== null) {
+            traverse = traverse.nextNode;
+        }
+        traverse.nextNode = null;
+    }
+
+    contains(value) {
+        let traverse = this.head;
+        while (traverse != null) {
+            if (traverse.value === value) {
+                return true;
+            }
+            traverse = traverse.nextNode;
+        }
+        return false;
+    }
+
+    find(value) {
+        let traverse = this.head;
+        let idx = 0;
+        while (traverse != null) {
+            if (traverse.value === value) {
+                return idx;
+            }
+            traverse = traverse.nextNode;
+            idx++;
+        }
+        return null;
+    }
+
+    toString() {
+        let traverse = this.head;
+        while (traverse !== null) {
+            console.log(`( ${traverse.value} ) -> `);
+            traverse = traverse.nextNode;
+        }
+        console.log('null');
+    }
+
+    insertAt(value, index) {
+        let traverse = this.head;
+        for (let i = 0; i < index; i++) {
+            traverse = traverse.nextNode;
+        }
+        let node = new Node(value, traverse);
+        traverse = this.head;
+        for (let i = 0; i < index - 1; i++) {
+            traverse = traverse.nextNode;
+        }
+        traverse.nextNode = node;
+    }
+
+    removeAt(index) {
+        let traverse = this.head;
+        for (let i = 0; i < index - 1; i++) {
+            traverse = traverse.nextNode;
+        }
+        traverse.nextNode = traverse.nextNode.nextNode;
+    }
+}
+
+export { LinkedList };
---
+++ main.js
@@ -0,0 +1,44 @@
+import { LinkedList } from "./app.js";
+
+const list = new LinkedList();
+
+list.prepend("dragon");
+list.append("monkey");
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
+
+console.log(list.toString());
+
+list.prepend("gorilla");
+
+console.log(list.toString());
+
+console.log(`the size of the list is ${list.size()}`);
+
+console.log(`the head of the list is ${list.getHead()}`);
+
+console.log(`the tail of the list is ${list.tail()}`);
+
+list.pop();
+
+console.log(list.toString());
+
+console.log(`the tail of the list is ${list.tail()}`);
+
+console.log(`the value of the list at index 4 is ${list.at(4)}`);
+
+console.log(`the statement that the list contains \"monkey\" is ${list.contains('monkey')}`);
+
+console.log(`the index of the list containing \"parrot\" is ${list.find('parrot')}`);
+
+list.insertAt('chicken', 5);
+
+console.log(list.toString());
+
+list.removeAt(5);
+
+console.log(list.toString());
